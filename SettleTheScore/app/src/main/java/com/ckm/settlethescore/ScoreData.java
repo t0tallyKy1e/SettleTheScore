9f653a16e03d75475a2e67b3886c999b180b45c5
Chris Berns <cberns223@gmail.com>
2018-12-05 10:31:58 -0500

    Scores Mocked Up for Demo

--- SettleTheScore/app/src/main/java/com/ckm/settlethescore/ScoreData.java
+++ SettleTheScore/app/src/main/java/com/ckm/settlethescore/ScoreData.java
@@ -1,5 +1,16 @@
 package com.ckm.settlethescore;
 
+import android.support.annotation.NonNull;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.IgnoreExtraProperties;
+import com.google.firebase.database.ValueEventListener;
+
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -11,15 +22,42 @@ public class ScoreData {
 
     public static final Map<String, ScoreItem> ITEM_MAP = new HashMap<String, ScoreItem>();
 
-    private static String[] sampleScores = { "Kyle Won: Paper beats Rock",
-                                                "Marvin rolled a 20",
-                                                "Chris: 20 life, Kyle: 18 life"};
+    private static String[] sampleScores = {"Kyle Won: Paper beats Rock",
+            "Marvin rolled a 20",
+            "Chris: 20 life, Kyle: 18 life"};
 
-    private static String[] sampleStates = { "Won", "Lost", "In Progress"};
-    private static final int COUNT = 6;
+    private static String[] sampleStates = {"Won", "Lost", "In Progress"};
+    private static int COUNT = 0;
+
+    private static ArrayList<SessionData> sessions = new ArrayList<SessionData>();
 
     static {
-        // Add some sample items.
+        final FirebaseDatabase database = FirebaseDatabase.getInstance();
+        final DatabaseReference databaseReference = database.getReference();
+        ValueEventListener postListener = new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for (DataSnapshot dataSnapshot2 : dataSnapshot.child("Sessions").getChildren()) {
+                    SessionData session = dataSnapshot2.getValue(SessionData.class);
+                    session.key = dataSnapshot2.getKey();
+                    sessions.add(session);
+                    ScoreItem score = new ScoreItem(
+                            session.key,
+                            session.game_type,
+                            sampleScores[COUNT++ % 3],
+                            session.host);
+                    addItem(score);
+
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        };
+
+        databaseReference.addValueEventListener(postListener);
         for (int i = 1; i <= COUNT; i++) {
             addItem(createScore(i));
         }
@@ -30,10 +68,6 @@ public class ScoreData {
         ITEM_MAP.put(item.id, item);
     }
 
-    private static ScoreItem createScore(int position) {
-        return new ScoreItem(String.valueOf(position), sampleScores[position % sampleScores.length], makeDetails(position), position);
-    }
-
     private static String makeDetails(int position) {
         StringBuilder builder = new StringBuilder();
         builder.append("Details about Item: ").append(position);
@@ -42,29 +76,38 @@ public class ScoreData {
         }
         return builder.toString();
     }
+
+    private static ScoreItem createScore(int position) {
+        return new ScoreItem(String.valueOf(position), sampleScores[position % sampleScores.length], makeDetails(position), "Unknown");
+    }
+
     /*
         Sample Score Item to be replaced with actual data
      */
     public static class ScoreItem {
+        public final String game_type;
         public final String id;
-        public final String content;
-        public final String details;
-        public final String gameStatus;
         public final String score;
-        public final int count;
+        public final String host;
 
-        public ScoreItem(String id, String content, String details, int count) {
+        public ScoreItem(String id, String game_type, String score, String host) {
             this.id = id;
-            this.content = "Player 1";
-            this.details = details;
-            this.gameStatus = "In Progress";
-            this.score = content;
-            this.count = count;
+            this.game_type = game_type;
+            this.score = score;
+            this.host = host;
         }
 
         @Override
         public String toString() {
-            return content;
+            return game_type;
         }
+    }
+
+    @IgnoreExtraProperties
+    public static class SessionData {
+        public String game_type;
+        public String host;
+        public String key;
+        public Integer dice_roll;
     }
 }
